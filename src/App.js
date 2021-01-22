b48cd65e18af9c6aa9b4d596cadb0cdc4084eaee
Yasir Hassan <hasanyasir01@gmail.com>
2021-01-22 12:03:24 -0800

    pass likes to LikeSection

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
+  const [posts , setPosts] = useState(dummyData) 
 
   const likePost = postId => {
     /*
@@ -36,7 +37,7 @@ const App = () => {
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
       <SearchBar />
-      <Posts likePost={likePost}  posts={dummyData} />
+      <Posts likePost={likePost}  posts={posts} />
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
